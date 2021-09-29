1d196ab5308e2d45746007092fa68a0bbce414de
Kovalev Andrey <kovalev1000000andrei@yandex.ru>
2021-09-29 19:23:50 +0300

    changes on allReports.rb and docRep.rb 29.09.21

--- docRep.rb
+++ docRep.rb
@@ -7,9 +7,11 @@ set :port, 4567
 
 get '/' do
     return "hello!
-    on path http://localhost:4567/price you can see price on vm!
-    on path  http://localhost:4567/price/dop you can see price on VM with additional hard drive"
+    on path http://localhost:4567/price you can see price on vm!"
 end
+
+
+
 
 
 get '/price' do
@@ -18,25 +20,15 @@ get '/price' do
   ram = params['ram']
   hdd_type = params['hdd_type']
   hdd_capacity = params['hdd_capacity']
- 
-  report = AllReports.new("./csv/vms.csv", "./csv/prices.csv", "./csv/volumes.csv")
-  return  report.price_vm(cpu, ram, hdd_type, hdd_capacity)
-  
-end
-# http://localhost:4567/price?cpu=16&ram=1&hdd_type=sata&hdd_capacity=800
-
-get '/price/dop' do
-  
-  cpu = params['cpu']
-  ram = params['ram']
-  hdd_type = params['hdd_type']
-  hdd_capacity = params['hdd_capacity']
   dop = params['dop']
 
-  report = AllReports.new("./csv/vms.csv", "./csv/prices.csv", "./csv/volumes.csv")
-  return  report.price_vm_with_dop(cpu, ram, hdd_type, hdd_capacity, dop)
+
+  report = AllReports.new
+  return  report.price_vm(cpu, ram, hdd_type, hdd_capacity, dop)
   
 end
 
-# http://localhost:4567/price/dop?cpu=16&ram=1&hdd_type=sata&hdd_capacity=800&dop[][type]=sata&dop[][value]=1000
-
+# examples of requests
+# http://localhost:4567/price?cpu=16&ram=1&hdd_type=sata&hdd_capacity=800
+# http://localhost:4567/price?cpu=16&ram=1&hdd_type=sata&hdd_capacity=800&dop[][type]=sata&dop[][value]=1000
+# http://localhost:4567/price?cpu=16&ram=1&hdd_type=sata&hdd_capacity=800&dop[][type]=sata&dop[][value]=1000&dop[][type]=sas&dop[][value]=1000
